5df3a637c0c61e8c595846c90a12ca5c9ca75d4f
David McCourt <david.faulkenberry@gmail.com>
2021-11-23 17:13:27 -0500

    No tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :scores
+  resources :race_results
+  resources :tracks
+  resources :races
+  resources :drivers
+  resources :points_allocations
+  resources :points_systems
+  resources :seasons
+  resources :championships
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
